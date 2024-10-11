ec8be6c7e387eb8ed89438aa9bade24526c408ed
Brijesh Kumar Mishra <46743531+mishrabrijesh240999@users.noreply.github.com>
2024-10-11 15:29:48 +0530

    added a comment suggestion on how to create this Game @ cwh_43_Ch9_GuessTheNumberGame_OOPsEdition.java

--- FirstJavaProgramCWH/src/com/company/cwh_43_Ch9_GuessTheNumberGame_OOPsEdition.java
+++ FirstJavaProgramCWH/src/com/company/cwh_43_Ch9_GuessTheNumberGame_OOPsEdition.java
@@ -2,6 +2,15 @@ package com.company;
 
 public class cwh_43_Ch9_GuessTheNumberGame_OOPsEdition {
     public static void main(String[] args) {
+        /*
+        Create a Class GuessTheNumberGame, which allows a user to play "Guess the todays lucky  number"
+        game once. Game should have the following methods:
+        1. Constructor to generate the random number
+        2. takeUserInput() to take a user Input of number
+        3. isCorrectNumber() to detect whether the number entered by the user is true
+        4. getter and setter for noOfGuesses
+        Use proporties suc as noOfGuesses(int), etc to get this task done!
+        */
 
     }
 }
